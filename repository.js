feece5c14e2e94665fb0e348057f639359cb495a
sharangw <sharang.wakankar@hotmail.com>
2022-04-21 15:24:14 +0000

    Add a mock event

--- repository.js
+++ repository.js
@@ -29,6 +29,7 @@ const mockEvents = {
     events: [
         { id: 1, title: 'a mock event', description: 'something really cool', location: 'Chez Joe Pizza', likes: 0, datetime_added: '2022-02-01:12:00' },
         { id: 2, title: 'another mock event', description: 'something even cooler', location: 'Chez John Pizza', likes: 0, datetime_added: '2022-02-01:12:00' },
+        { id: 3, title: 'yet another mock event', description: 'something warmer', location: 'Papa John Pizza', likes: 0, datetime_added: '2022-02-01:12:00' },
     ]
 };
 
